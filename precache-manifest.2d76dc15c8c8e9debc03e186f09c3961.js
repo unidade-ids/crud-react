0b64a9327d1fa39cafc4cf2b3ca5ddf0d2bc8f8d
unidade <unidade.ids@gmail.com>
2020-07-30 20:40:14 -0300

    Updates

---
+++ precache-manifest.2d76dc15c8c8e9debc03e186f09c3961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5670286a5985c907c11394fb0d2984d9",
+    "url": "/unidade-ids/crud-react/index.html"
+  },
+  {
+    "revision": "e76b524a104a8b521c96",
+    "url": "/unidade-ids/crud-react/static/css/main.d1e1a33d.chunk.css"
+  },
+  {
+    "revision": "4658d1b0a7a2955be83d",
+    "url": "/unidade-ids/crud-react/static/js/2.08e02969.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unidade-ids/crud-react/static/js/2.08e02969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76b524a104a8b521c96",
+    "url": "/unidade-ids/crud-react/static/js/main.f3f606fc.chunk.js"
+  },
+  {
+    "revision": "db0edd0f9c833b06eae3",
+    "url": "/unidade-ids/crud-react/static/js/runtime-main.5f7bea6e.js"
+  }
+]);
